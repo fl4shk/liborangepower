21ba9ebfb51f41a041e27dee5d13abbf36f3836e
fl4shk <fl4shk@users.noreply.github.com>
2017-01-30 14:22:34 -0500

    Add license

--- src/misc_includes.hpp
+++ src/misc_includes.hpp
@@ -1,3 +1,22 @@
+// This file is part of Lib Orange Power.
+// 
+// Copyright (C) 2017 Andrew Clark (FL4SHK)
+// 
+// Lib Orange Power is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or (at
+// your option) any later version.
+// 
+// Lib Orange Power is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+// General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with Lib Orange Power.  If not, see
+// <http://www.gnu.org/licenses/>.
+
+
 #ifndef misc_includes_hpp
 #define misc_includes_hpp
 
