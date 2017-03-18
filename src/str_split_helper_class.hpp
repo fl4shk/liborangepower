fcdaa28a32eaee059e2bf81105d9826d5ced0916
fl4shk <fl4shk@users.noreply.github.com>
2017-03-18 15:56:49 -0400

    Allow more than just '"' as endpoints of token

--- src/str_split_helper_class.hpp
+++ src/str_split_helper_class.hpp
@@ -6,22 +6,106 @@
 #include "misc_includes.hpp"
 
 // str_type might be an std::string, an std::string_view, etc.  It just
-// needs to support the at() and size() member functions in the same way
-// that std::string and std::string_view do.
+// needs to support the at() and size() member functions, and the
+// value_type, in the same way that std::string and std::string_view do.
 template< typename str_type >
 class str_split_helper
 {
+public:		// typedefs
+	typedef typename str_type::value_type value_type;
+	typedef std::pair< value_type, value_type > val_typ_pair;
+	//using decay_t = typename std::decay<value_type&&>::type;
+	
 protected:		// variables
 	str_type internal_to_split;
 	size_t internal_line_num = 1;
 	
 protected:		// functions
+	static inline auto make_vt_pair( const value_type& t, 
+		const value_type& u )
+	{
+		return std::make_pair< value_type, value_type >( t, u );
+	}
+	static inline  auto make_vt_pair( value_type&& t, value_type&& u )
+	{
+		return std::make_pair< value_type, value_type >( std::move(t), 
+			std::move(u) );
+	}
+	
 	// This function should be overrided by derived classes
-	virtual bool to_split_at_is_special_char( size_t i ) const = 0;
+	// 
+	// Special characters are those that are NOT pairs, but just individual
+	// characters that should become their own words.
+	virtual bool to_split_at_is_indiv_word( size_t i ) const = 0;
 	//{
 	//	//return ( to_split().at(i) == ',' );
 	//	return false;
 	//}
+	
+	
+	// This function CAN be overrided by derived classes, but it doesn't
+	// have to be
+	virtual const std::vector<val_typ_pair>
+		list_of_special_endpoint_pairs() const
+	{
+		std::vector<val_typ_pair> ret;
+		
+		ret.push_back(make_vt_pair( '"', '"' ));
+		
+		//// Example of something that might be helpful for derived
+		////classes:
+		//ret.push_back(make_vt_pair( '[', ']' ));
+		
+		return ret;
+	}
+	
+	bool to_split_at_is_special_first_endpoint( size_t i, 
+		val_typ_pair& which ) const
+	{
+		const auto& temp = list_of_special_endpoint_pairs();
+		
+		for ( auto iter : temp )
+		{
+			if ( to_split().at(i) == iter.first )
+			{
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	bool to_split_at_is_special_second_endpoint( size_t i,
+		val_typ_pair& which ) const
+	{
+		const auto& temp = list_of_special_endpoint_pairs();
+		
+		for ( auto iter : temp )
+		{
+			if ( to_split().at(i) == iter.second )
+			{
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	bool to_split_at_is_any_special_endpoint( size_t i ) const
+	{
+		const auto& temp = list_of_special_endpoint_pairs();
+		
+		for ( auto iter : temp )
+		{
+			if ( ( to_split().at(i) == iter.first  )
+				|| ( to_split().at(i) == iter.second ) )
+			{
+				return true;
+			}
+		}
+		
+		return false;
+	}
 	
 	
 public:		// functions
@@ -84,8 +168,10 @@ public:		// functions
 	// This function is NOT meant to be overrided by derived classes
 	bool to_split_at_is_end_of_word( size_t i ) const
 	{
-		return ( to_split_at_is_special_char(i)
-			|| ( to_split().at(i) == '"' ) );
+		//return ( to_split_at_is_indiv_word(i)
+		//	|| ( to_split().at(i) == '"' ) );
+		return ( to_split_at_is_indiv_word(i)
+			|| to_split_at_is_any_special_endpoint(i) );
 	}
 	
 	static inline size_t get_substr_size( const size_t start, 
@@ -108,7 +194,6 @@ public:		// functions
 		// Eat white space (nom)
 		while ( index_in_to_split(i) && isspace(to_split().at(i)) )
 		{
-			//++i;
 			increment_i(i);
 		}
 		return i;
@@ -118,15 +203,33 @@ public:		// functions
 	{
 		if ( index_in_to_split(i) )
 		{
-			// Handle special characters
-			if ( to_split_at_is_special_char(i) )
+			val_typ_pair temp;
+			
+			if ( to_split_at_is_indiv_word(i) )
 			{
 				increment_i(i);
 			}
-			else if ( to_split().at(i) == '"' )
+			//else if ( to_split().at(i) == '"' )
+			//{
+			//	increment_i(i);
+			//	while ( index_in_to_split(i) && to_split().at(i) != '"' )
+			//	{
+			//		increment_i(i);
+			//	}
+			//	
+			//	// The pos after end needs to be after the quote character
+			//	// because both the starting and ending quote characters
+			//	// are considered part of the so-called "word"
+			//	if ( index_in_to_split(i) )
+			//	{
+			//		increment_i(i);
+			//	}
+			//}
+			else if ( to_split_at_is_special_first_endpoint( i, temp ) )
 			{
 				increment_i(i);
-				while ( index_in_to_split(i) && to_split().at(i) != '"' )
+				while ( index_in_to_split(i) 
+					&& to_split().at(i) != temp.second )
 				{
 					increment_i(i);
 				}
@@ -147,7 +250,6 @@ public:		// functions
 				while ( index_in_to_split(i) && !isspace(to_split().at(i))
 					&& !to_split_at_is_end_of_word(i) )
 				{
-					//++i;
 					increment_i(i);
 				}
 			}
