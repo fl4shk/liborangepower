7c9603a028bef05033477af4d42350a8f387ef9e
fl4shk <fl4shk@users.noreply.github.com>
2017-05-19 12:57:03 -0500

    static gen getters and setters

--- src/gen_getter_setter_defines.hpp
+++ src/gen_getter_setter_defines.hpp
@@ -55,6 +55,56 @@ gen_generic_getter_by_ref( internal, suffix )
 
 
 
+#ifndef gen_static_generic_getter_by_val
+// By value
+#define gen_static_generic_getter_by_val( prefix, suffix ) \
+static inline decltype(prefix##_##suffix) suffix() const \
+{ \
+	return prefix##_##suffix; \
+}
+#endif
+
+#ifndef gen_static_getter_by_val
+// By value
+#define gen_static_getter_by_val( suffix ) \
+gen_static_generic_getter_by_val( internal, suffix )
+#endif
+
+
+
+#ifndef gen_static_generic_getter_by_con_ref
+// By constant reference
+#define gen_static_generic_getter_by_con_ref( prefix, suffix ) \
+static inline const decltype(prefix##_##suffix)& suffix() const \
+{ \
+	return prefix##_##suffix; \
+}
+#endif
+
+#ifndef gen_static_getter_by_con_ref
+// By constant reference
+#define gen_static_getter_by_con_ref( suffix ) \
+gen_static_generic_getter_by_con_ref( internal, suffix )
+#endif
+
+
+
+#ifndef gen_static_generic_getter_by_ref
+// By reference
+#define gen_static_generic_getter_by_ref( prefix, suffix ) \
+static inline decltype(prefix##_##suffix)& suffix() \
+{ \
+	return prefix##_##suffix; \
+}
+#endif
+
+#ifndef gen_static_getter_by_ref
+// By reference
+#define gen_static_getter_by_ref( suffix ) \
+gen_static_generic_getter_by_ref( internal, suffix )
+#endif
+
+
 // Setters
 
 #ifndef gen_generic_setter_by_val
@@ -112,9 +162,59 @@ gen_generic_setter_by_rval_ref( internal, suffix )
 #endif
 
 
-#ifndef gen_enum_ostm_sl_decl
-#define gen_enum_ostm_sl_decl
+
+#ifndef gen_static_generic_setter_by_val
+// By value
+#define gen_static_generic_setter_by_val( prefix, suffix ) \
+static inline decltype(prefix##_##suffix) set_##suffix( decltype(prefix##_##suffix) to_copy ) \
+{ \
+	prefix##_##suffix = to_copy; \
+	return prefix##_##suffix; \
+}
 #endif
 
+#ifndef gen_static_setter_by_val
+// By value
+#define gen_static_setter_by_val( suffix ) \
+gen_static_generic_setter_by_val( internal, suffix )
+#endif
+
+
+
+#ifndef gen_static_generic_setter_by_con_ref
+// By const reference
+#define gen_static_generic_setter_by_con_ref( prefix, suffix ) \
+static inline const decltype(prefix##_##suffix)& set_##suffix \
+	( const decltype(prefix##_##suffix)& to_copy ) \
+{ \
+	prefix##_##suffix = to_copy; \
+	return prefix##_##suffix; \
+}
+#endif
+
+#ifndef gen_static_setter_by_con_ref
+// By const reference
+#define gen_static_setter_by_con_ref( suffix ) \
+gen_static_generic_setter_by_con_ref( internal, suffix )
+#endif
+
+
+
+#ifndef gen_static_generic_setter_by_rval_ref
+// By rvalue reference
+#define gen_static_generic_setter_by_rval_ref( prefix, suffix ) \
+static inline const decltype(prefix##_##suffix)& set_##suffix \
+	( decltype(prefix##_##suffix)&& to_move ) \
+{ \
+	prefix##_##suffix = std::move(to_move); \
+	return prefix##_##suffix; \
+}
+#endif
+
+#ifndef gen_static_setter_by_rval_ref
+// By rvalue reference
+#define gen_static_setter_by_rval_ref( suffix ) \
+gen_static_generic_setter_by_rval_ref( internal, suffix )
+#endif
 
 #endif		// liborangepower_gen_getter_setter_defines_hpp
