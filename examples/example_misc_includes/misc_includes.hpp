e5586b8470d4c1c5ea6b9877ab507b74c6c3c0c7
fl4shk <fl4shk@users.noreply.github.com>
2017-08-21 18:26:01 -0400

    Example misc_includes.hpp

---
+++ examples/example_misc_includes/misc_includes.hpp
@@ -0,0 +1,22 @@
+#ifndef misc_includes_hpp
+#define misc_includes_hpp
+
+
+
+
+#include "liborangepower_src/misc_includes.hpp"
+#include "liborangepower_src/misc_output_classes.hpp"
+#include "liborangepower_src/misc_types.hpp"
+#include "liborangepower_src/misc_utility_funcs.hpp"
+#include "liborangepower_src/misc_bitwise_funcs.hpp"
+#include "liborangepower_src/misc_defines.hpp"
+#include "liborangepower_src/gen_getter_setter_defines.hpp"
+
+using namespace liborangepower::printout_etc;
+using namespace liborangepower::integer_types;
+using namespace liborangepower::misc_util;
+using namespace liborangepower::bitwise;
+
+
+
+#endif		// misc_includes_hpp
