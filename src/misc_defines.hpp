8542ba7c667a532aece344341a3851817bc54913
fl4shk <fl4shk@users.noreply.github.com>
2019-01-10 17:15:44 -0600

    TOKENPASTE stuff

--- src/misc_defines.hpp
+++ src/misc_defines.hpp
@@ -19,4 +19,14 @@
 	asm volatile("//@ ---" stuff " ---")
 
 
+#define INNER_TOKENPASTE1(x) x
+#define TOKENPASTE1(x) INNER_TOKENPASTE1(x)
+
+#define INNER_TOKENPASTE2(x, y) x ## y
+#define TOKENPASTE2(x, y) INNER_TOKENPASTE2(x, y)
+
+#define TOKENPASTE3(x, y, z) TOKENPASTE2(x, TOKENPASTE2(y, z))
+#define TOKENPASTE4(x, y, z, w) TOKENPASTE3(x, y, TOKENPASTE2(z, w))
+
+
 #endif		// liborangepower_misc_defines_hpp
