b0ee8944e10e28d55093a40851eea4bf59af7429
fl4shk <fl4shk@users.noreply.github.com>
2019-03-02 09:50:54 -0600

    stuffs

--- src/json_stuff.hpp
+++ src/json_stuff.hpp
@@ -26,20 +26,20 @@ void err(arg_types&&... args)
 std::string get_json_value_type_as_str(const Json::Value& some_value);
 inline std::string get_json_value_type_as_str(Json::Value* iter)
 {
-	return std::move(get_json_value_type_as_str(*iter));
+	return get_json_value_type_as_str(*iter);
 }
 
 inline void print_json_value_type(const Json::Value& some_value,
 	std::ostream& os=std::cout)
 {
 	printout_etc::osprintout(os, 
-		std::move(get_json_value_type_as_str(some_value)));
+		get_json_value_type_as_str(some_value));
 }
 inline void print_json_value_type(Json::Value* iter, 
 	std::ostream& os=std::cout)
 {
 	printout_etc::osprintout(os, 
-		std::move(get_json_value_type_as_str(iter)));
+		get_json_value_type_as_str(iter));
 }
 
 bool parse_json(Json::CharReaderBuilder* rbuilder, std::istream& is,
