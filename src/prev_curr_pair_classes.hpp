94006560827345a1bdce47d4c3463568dd9be4a6
fl4shk <fl4shk@users.noreply.github.com>
2019-03-17 20:15:55 -0500

    prev_curr_pair stuff

---
+++ src/prev_curr_pair_classes.hpp
@@ -0,0 +1,116 @@
+#ifndef liborangepower_prev_curr_pair_classes_hpp
+#define liborangepower_prev_curr_pair_classes_hpp
+
+#include "misc_types.hpp"
+#include "misc_defines.hpp"
+
+#include <utility>
+
+#include "gen_class_innards_defines.hpp"
+
+
+namespace liborangepower
+{
+
+namespace containers
+{
+
+// This is a small class for the purposes of holding the states of
+// "previous" and "current" pairs.  It is primarily intended for use with
+// SMALL types, such as vec2's, built-in types, and so on and so forth.
+template<typename Type>
+class PrevCurrPair
+{
+protected:		// variables
+	Type _prev, _curr;
+
+public:		// functions
+	inline PrevCurrPair() = default;
+
+	GEN_CM_BOTH_CONSTRUCTORS_AND_ASSIGN(PrevCurrPair);
+	virtual ~PrevCurrPair() = default;
+
+
+	inline void next()
+	{
+		_prev = curr;
+	}
+
+	inline void next_and_update(const Type& n_curr)
+	{
+		_prev = std::move(curr);
+		_curr = n_curr;
+	}
+	inline void next_and_update(Type&& n_curr)
+	{
+		_prev = std::move(_curr);
+		_curr = std::move(n_curr);
+	}
+
+	inline bool has_changed() const
+	{
+		return (_prev != _curr);
+	}
+
+	inline Type& operator () ()
+	{
+		return _curr;
+	}
+	inline const Type& operator () () const
+	{
+		return _curr;
+	}
+
+	GEN_GETTER_BY_CON_REF(prev)
+	GEN_GETTER_BY_CON_REF(curr)
+};
+
+
+template<typename Type>
+class MoveOnlyPrevCurrPair
+{
+protected:		// variables
+	Type _prev, _curr;
+	
+public:		// functions
+	inline MoveOnlyPrevCurrPair() = default;
+	GEN_MOVE_ONLY_CONSTRUCTORS_AND_ASSIGN(MoveOnlyPrevCurrPair);
+	virtual ~MoveOnlyPrevCurrPair() = default;
+
+	inline void next()
+	{
+		_prev = std::move(_curr);
+		_curr = Type();
+	}
+
+	inline void next_and_update(Type&& n_curr)
+	{
+		_prev = std::move(_curr);
+		_curr = std::move(n_curr);
+	}
+
+	inline bool has_changed() const
+	{
+		return (_prev != _curr);
+	}
+
+	inline Type& operator () ()
+	{
+		return _curr;
+	}
+	inline const Type& operator () () const
+	{
+		return _curr;
+	}
+
+	GEN_GETTER_BY_CON_REF(prev)
+	GEN_GETTER_BY_CON_REF(curr)
+};
+
+
+} // namespace containers
+
+} // namespace liborangepower
+
+
+#endif		// liborangepower_prev_curr_pair_classes_hpp
--- src/vec2_classes.hpp
+++ src/vec2_classes.hpp
@@ -1,6 +1,8 @@
 #ifndef liborangepower_vec2_classes_hpp
 #define liborangepower_vec2_classes_hpp
 
+#include "gen_class_innards_defines.hpp"
+
 namespace liborangepower
 {
 
@@ -14,19 +16,15 @@ public:		// variables
 	Type x, y;
 
 public:		// functions
-	inline Vec2()
-	{
-	}
+	inline Vec2() = default;
 	inline Vec2(const Type& s_x, const Type& s_y)
 		: x(s_x), y(s_y)
 	{
 	}
 
-	inline Vec2(const Vec2& to_copy) = default;
-	inline Vec2(Vec2&& to_move) = default;
+	GEN_CM_BOTH_CONSTRUCTORS_AND_ASSIGN(Vec2);
+	virtual ~Vec2() = default;
 
-	inline Vec2& operator = (const Vec2& to_copy) = default;
-	inline Vec2& operator = (Vec2&& to_move) = default;
 };
 
 } // namespace containers
