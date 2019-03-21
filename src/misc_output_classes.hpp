6a525e9079be1fcb41bcf6ed2a403549656330d3
fl4shk <fl4shk@users.noreply.github.com>
2019-03-21 17:32:54 -0500

    stuffs

--- src/misc_output_classes.hpp
+++ src/misc_output_classes.hpp
@@ -21,8 +21,8 @@ private:		// functions
 	static void func(std::ostream& os, const FirstType& first_val, 
 		RemArgTypes&&... rem_args)
 	{
-		typedef std::remove_reference<FirstType>::type Temp0;
-		typedef std::remove_const<Temp0>::type Temp1;
+		typedef typename std::remove_reference<FirstType>::type Temp0;
+		typedef typename std::remove_const<Temp0>::type Temp1;
 		static_assert(!std::is_same<std::string*, Temp1>());
 		os << first_val;
 
