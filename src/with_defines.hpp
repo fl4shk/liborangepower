6f5317af632c891278e6402548b31dc2972714cf
fl4shk <fl4shk@users.noreply.github.com>
2019-04-13 14:36:03 -0500

    stuffs

---
+++ src/with_defines.hpp
@@ -0,0 +1,7 @@
+#ifndef liborangepower_with_defines_hpp
+#define liborangepower_with_defines_hpp
+
+#define with(obj) if (auto obj; true)
+#define pwith(obj) if (obj; true)
+
+#endif		// liborangepower_with_defines_hpp
