3e63729234b51db268e5330628124414877f7f62
fl4shk <fl4shk@users.noreply.github.com>
2019-04-12 20:12:17 -0500

    stuffs

--- src/misc_defines.hpp
+++ src/misc_defines.hpp
@@ -4,11 +4,11 @@
 
 #define WIDTH2MP(some_width) ((some_width) - 1)
 #define BPRANGE2MASK(bit_pos_hi, bit_pos_lo) \
-	(((size_t)1 << ((size_t)bit_pos_hi - (size_t)bit_pos_lo + (size_t)1)) \
-	- (size_t)1)
+	(((size_t)1 << (((size_t)bit_pos_hi) - (size_t)(bit_pos_lo) \
+	+ (size_t)1)) - (size_t)1)
 #define BPRANGE2SHIFTED_MASK(bit_pos_hi, bit_pos_lo) \
-	((((size_t)1 << ((size_t)bit_pos_hi - (size_t)bit_pos_lo \
-	+ (size_t)1)) - (size_t)1) << (size_t)bit_pos_lo)
+	((((size_t)1 << ((size_t)(bit_pos_hi) - (size_t)(bit_pos_lo) \
+	+ (size_t)1)) - (size_t)1) << (size_t)(bit_pos_lo))
 
 #define _ALIGN4 aligned(4)
 #define _ALIGN2 aligned(2)
