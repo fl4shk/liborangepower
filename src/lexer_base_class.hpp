0bdf845ce6e8840ff13e95fae5ef9813057abb52
fl4shk <fl4shk@users.noreply.github.com>
2019-06-29 06:45:07 -0500

    lang stuff

---
+++ src/lexer_base_class.hpp
@@ -0,0 +1,236 @@
+#ifndef liborangepower_lexer_base_class_hpp
+#define liborangepower_lexer_base_class_hpp
+
+// src/lexer_base_class.hpp
+
+#include "misc_includes.hpp"
+#include "src_code_chunk_class.hpp"
+
+namespace liborangepower
+{
+
+namespace lang
+{
+
+template<typename _TokType>
+class LexerBase
+{
+public:		// types
+	using TokType = _TokType;
+
+	class State
+	{
+	public:		// variables
+		TokType tok;
+		string s;
+		int c;
+		BigNum n;
+		size_t x = 0, line_num = 1, pos_in_line = 0;
+
+	public:		// functions
+		State()
+		{
+		}
+		GEN_CM_BOTH_CONSTRUCTORS_AND_ASSIGN(State)
+		~State()
+		{
+		}
+
+		// This is just for sorting purposes (mainly for `std::set`)
+		bool operator < (const State& other) const
+		{
+			return (x < other.x);
+			//std::map<string, bool> cmp_lt_results, cmp_eq_results;
+			//cmp_lt_results["tok"] = (tok < other.tok);
+			//cmp_eq_results["tok"] = (tok == other.tok);
+
+			//cmp_lt_results["s"] = (s < other.s);
+			//cmp_eq_results["s"] = (s == other.s);
+
+			//cmp_lt_results["c"] = (c < other.c);
+			//cmp_eq_results["c"] = (c == other.c);
+
+			//cmp_lt_results["n"] = (n < other.n);
+			//cmp_eq_results["n"] = (n == other.n);
+
+			//cmp_lt_results["x"] = (x < other.x);
+			//cmp_eq_results["x"] = (x == other.x);
+
+			//cmp_lt_results["line_num"] = (line_num < other.line_num);
+			//cmp_eq_results["line_num"] = (line_num == other.line_num);
+
+			//cmp_lt_results["pos_in_line"]
+			//	= (pos_in_line < other.pos_in_line);
+			//cmp_eq_results["pos_in_line"]
+			//	= (pos_in_line == other.pos_in_line);
+
+			//for (const auto& iter : cmp_lt_results)
+			//{
+			//	if (iter.second)
+			//	{
+			//		return true;
+			//	}
+			//	else if (cmp_eq_results[iter.first])
+			//	{
+			//		continue;
+			//	}
+			//	break;
+			//}
+			//return false;
+
+			//return (tok < other.tok);
+		}
+		bool operator == (const State& other) const
+		{
+			return (x == other.x);
+			//return ((tok == other.tok) && (s == other.s) && (c == other.c)
+			//	&& (n == other.n) && (x == other.x)
+			//	&& (line_num == other.line_num)
+			//	&& (pos_in_line == other.pos_in_line));
+			//return (tok == other.tok);
+		}
+	};
+
+protected:		// variables
+	string _filename;
+	string* _text = nullptr;
+	State _state;
+
+public:		// functions
+	inline LexerBase(const string& s_filename, string* s_text,
+		TokType done_tok, TokType comment_tok)
+		: _filename(s_filename), _text(s_text)
+	{
+		_next_tok_no_test(done_tok, comment_tok);
+	}
+	GEN_CM_BOTH_CONSTRUCTORS_AND_ASSIGN(LexerBase)
+	virtual ~LexerBase()
+	{
+	}
+
+
+	inline auto src_code_chunk(State* state=nullptr) const
+	{
+		if (state == nullptr)
+		{
+			return SrcCodeChunk(filename(), "", state->line_num,
+				state->pos_in_line);
+		}
+		else
+		{
+			return SrcCodeChunk(filename(), "", line_num(), pos_in_line());
+		}
+	}
+
+	inline decltype(_state.tok) tok() const
+	{
+		return _state.tok;
+	}
+	inline const decltype(_state.s)& s() const
+	{
+		return _state.s;
+	}
+	inline decltype(_state.c) c() const
+	{
+		return _state.c;
+	}
+	inline const decltype(_state.n) n() const
+	{
+		return _state.n;
+	}
+	inline decltype(_state.x) x() const
+	{
+		return _state.x;
+	}
+	inline decltype(_state.line_num) line_num() const
+	{
+		return _state.line_num;
+	}
+	inline decltype(_state.pos_in_line) pos_in_line() const
+	{
+		return _state.pos_in_line;
+	}
+	GEN_GETTER_BY_CON_REF(filename)
+	GEN_GETTER_BY_VAL(text)
+	GEN_GETTER_BY_CON_REF(state)
+
+protected:		// functions
+	TokType _next_tok_no_test(TokType done_tok, TokType comment_tok)
+	{
+		do
+		{
+			_eat_whitespace();
+
+			if (c() == EOF)
+			{
+				_set_tok(done_tok, false);
+			}
+			else
+			{
+				_inner_next_tok();
+			}
+		} while (tok() == comment_tok);
+
+		return tok();
+	}
+	int _next_char()
+	{
+		if (x() < text()->size())
+		{
+			_state.c = text()->at(_state.x++);
+
+			++_state.pos_in_line;
+
+			if (c() == '\n')
+			{
+				++_state.line_num;
+				_state.pos_in_line = 1;
+			}
+
+			return c();
+		}
+		else
+		{
+			return EOF;
+		}
+	}
+	void _eat_whitespace()
+	{
+		while (isspace(c()))
+		{
+			_next_char();
+		}
+	}
+	void _set_tok(TokType n_tok, bool perf_next_char)
+	{
+		_state.tok = n_tok;
+
+		if (perf_next_char)
+		{
+			_next_char();
+		}
+	}
+
+	virtual void _inner_next_tok() = 0;
+
+	inline bool _set_kw_tok
+		(const std::map<TokType, string>& some_tok_ident_map)
+	{
+		for (const auto& iter : some_tok_ident_map)
+		{
+			if (s() == iter.second)
+			{
+				_set_tok(iter.first, false);
+				return true;
+			}
+		}
+		return false;
+	}
+};
+
+} // namespace lang
+
+} // namespace liborangepower
+
+
+#endif		// liborangepower_lexer_base_class_hpp
---
+++ src/parser_base_class.hpp
@@ -0,0 +1,278 @@
+#ifndef liborangepower_parser_base_class_hpp
+#define liborangepower_parser_base_class_hpp
+
+// src/parser_base_class.hpp
+
+#include "misc_includes.hpp"
+#include "lexer_base_class.hpp"
+
+namespace liborangepower
+{
+
+namespace lang
+{
+
+template<typename LexerType>
+class ParserBase
+{
+public:		// types
+	using TokType = typename LexerType::TokType;
+	using LexerState = typename LexerType::State;
+	using TokToStringMap = std::map<TokType, string>;
+
+	class LexStateSets final
+	{
+	public:		// variables
+		std::set<const LexerState> found_set, dup_set;
+
+	public:		// functions
+		LexStateSets()
+		{
+		}
+		GEN_CM_BOTH_CONSTRUCTORS_AND_ASSIGN(LexStateSets)
+		~LexStateSets()
+		{
+		}
+
+		inline bool valid() const
+		{
+			return (dup_set.size() == 0);
+		}
+	};
+
+protected:		// variables
+	bool _just_test = false;
+	size_t _curr_file_index = 0;
+	std::vector<string> _filename_vec;
+	std::vector<string*> _text_vec;
+	std::vector<LexerType> _lexer_vec;
+
+public:		// functions
+	ParserBase(std::vector<string>&& s_filename_vec)
+		: _filename_vec(std::move(s_filename_vec))
+	{
+	}
+	GEN_CM_BOTH_CONSTRUCTORS_AND_ASSIGN(ParserBase)
+	virtual ~ParserBase()
+	{
+	}
+
+	//inline auto lex_tok() const
+	//{
+	//	return _lexer().tok();
+	//}
+	//inline const auto& lex_s() const
+	//{
+	//	return _lexer().s();
+	//}
+	//inline auto lex_src_code_chunk() const
+	//{
+	//	return _lexer().src_code_chunk();
+	//}
+	//inline bool cmp_lex_tok(TokType tok) const
+	//{
+	//	return (lex_tok() == tok);
+	//}
+
+
+	//// Order does not matter
+	//inline bool cmp_prefix_tok_list(const std::set<TokType>& prefix_set,
+	//	TokType end, bool& found_dup) const
+	//{
+	//	found_dup = false;
+
+	//	std::set<TokType> found_prefix_set;
+	//	Lexer temp_lexer(_lexer());
+	//	auto* lexer = &temp_lexer;
+
+	//	for (const auto& iter : prefix_set)
+	//	{
+	//		if (found_prefix_set.find)
+	//		_next_tok(lexer);
+	//	}
+	//}
+
+	inline bool cmp_lex_tok(TokType to_cmp) const
+	{
+		return (_lexer().tok() == to_cmp);
+	}
+
+	GEN_GETTER_BY_VAL(curr_file_index)
+	GEN_GETTER_BY_VAL(just_test)
+
+protected:		// functions
+	const LexerType& _lexer() const
+	{
+		return _lexer_vec.at(curr_file_index());
+	}
+	LexerType& _lexer()
+	{
+		return _lexer_vec.at(curr_file_index());
+	}
+	const LexerState& _lex_state() const
+	{
+		return _lexer().state();
+	}
+
+	const std::vector<const LexerState> _next_n_tokens(size_t n,
+		bool affect_lexer)
+	{
+		std::vector<const LexerState> ret;
+
+		LexerType temp_lexer(_lexer());
+
+		auto* lexer = (!affect_lexer) ? &_lexer() : &temp_lexer;
+
+		for (size_t i=0; i<n; ++i)
+		{
+			ret.push_back(lexer->state());
+			_next_tok(lexer);
+		}
+
+		return ret;
+	}
+
+	// Duplicates are banned.
+	bool _check_prefixed_tok_seq(const std::set<TokType>& prefix_set,
+		TokType end, LexStateSets& lss)
+	{
+		const auto tokens = _next_n_tokens((prefix_set.size() + 1),
+			!just_test());
+
+		for (const auto& tok_iter : tokens)
+		{
+			auto update_lss = [&]() -> void
+			{
+				if (lss.found_set.contains(tok_iter))
+				{
+					lss.dup_set.insert(tok_iter);
+				}
+				lss.found_set.insert(tok_iter);
+			};
+
+			if (prefix_set.contains(tok_iter.tok))
+			{
+				update_lss();
+			}
+			else if (tok_iter.tok == end)
+			{
+				update_lss();
+				return true;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		return false;
+	}
+
+
+	inline auto _next_tok(LexerType* lexer=nullptr)
+	{
+		if (lexer == nullptr)
+		{
+			return _lexer().next_tok(just_test());
+		}
+		else // if (lexer != nullptr)
+		{
+			return lexer->next_tok(just_test());
+		}
+	}
+
+	template<typename FirstFuncType, typename... RemFuncTypes>
+	bool _do_parse(FirstFuncType&& first_func, RemFuncTypes&&... rem_funcs)
+	{
+		_just_test = false;
+		if (std::mem_fn(first_func)(*this))
+		{
+			_just_test = true;
+			std::mem_fn(first_func)(*this);
+
+			return true;
+		}
+		else if constexpr (sizeof...(rem_funcs) != 0)
+		{
+			return _do_parse(rem_funcs...);
+		}
+
+		return false;
+	}
+
+	//inline void _syntax_error(const string& msg) const
+	//{
+	//	lex_src_code_chunk().syntax_error(msg);
+	//}
+	template<typename... ArgTypes>
+	inline void _err(const LexerState* lex_state, ArgTypes&&... args)
+	{
+		_lexer().src_code_chunk(lex_state).err(sconcat(args...));
+	}
+	template<typename... ArgTypes>
+	inline void _err(ArgTypes&&... args) const
+	{
+		_lexer().src_code_chunk().err(sconcat(args...));
+	}
+	template<typename... ArgTypes>
+	inline void _warn(const LexerState* lex_state, ArgTypes&&... args)
+	{
+		_lexer().src_code_chunk(lex_state).warn(sconcat(args...));
+	}
+	template<typename... ArgTypes>
+	inline void _warn(ArgTypes&&... args) const
+	{
+		_lexer().src_code_chunk().warn(sconcat(args...));
+	}
+
+	string _msg_for_expect(TokType tok,
+		const TokToStringMap& some_tok_ident_map,
+		const LexerState* lex_state=nullptr) const
+	{
+		return sconcat("Expected token ", some_tok_ident_map.at(tok),
+			".  Have token ", some_tok_ident_map.at(lex_state->tok),
+			" and string ", lex_state->s);
+	}
+	void _expect(TokType tok, const TokToStringMap& some_tok_ident_map,
+		const LexerState* lex_state=nullptr)
+	{
+		if (tok)
+		{
+			_err(_msg_for_expect(tok, some_tok_ident_map, lex_state));
+		}
+		_next_tok();
+	}
+	void _unexpected(const TokToStringMap& some_tok_ident_map,
+		const LexerState* some_lex_state=nullptr)
+	{
+		const LexerState* lex_state = (some_lex_state == nullptr)
+			? &_lexer().state() : some_lex_state;
+		_err("Unexpected token ", some_tok_ident_map.at(lex_state->tok),
+			".  Have string ", lex_state->s);
+	}
+	bool _to_next_in_list(TokType end, TokType separator,
+		const TokToStringMap& some_tok_ident_map)
+	{
+		if (cmp_lex_tok(end))
+		{
+			_next_tok();
+			return false;
+		}
+		else if (cmp_lex_tok(separator))
+		{
+			_next_tok();
+			return true;
+		}
+		else
+		{
+			_unexpected(some_tok_ident_map);
+		}
+	}
+};
+
+} // namespace lang
+
+} // namespace liborangepower
+
+
+#endif		// liborangepower_parser_base_class_hpp
