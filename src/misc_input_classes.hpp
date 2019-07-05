1b3f1966037f024146dd30a72ae35c81b2f9f72e
fl4shk <fl4shk@users.noreply.github.com>
2019-07-05 16:19:17 -0500

    stuffs

--- src/misc_input_classes.hpp
+++ src/misc_input_classes.hpp
@@ -31,34 +31,34 @@ inline std::string get_istream_as_str(std::istream& to_get_from)
 
 	return ret;
 }
-inline std::string get_istream_as_str(std::ifstream& to_get_from)
-{
-	std::string ret;
-
-	int c;
-	for (;;)
-	{
-		c = to_get_from.get();
-		if (!to_get_from.eof())
-		{
-			ret += c;
-		}
-		else // if (to_get_from.eof())
-		{
-			break;
-		}
-	}
-
-	return ret;
-}
+//inline std::string get_istream_as_str(std::ifstream& to_get_from)
+//{
+//	std::string ret;
+//
+//	int c;
+//	for (;;)
+//	{
+//		c = to_get_from.get();
+//		if (!to_get_from.eof())
+//		{
+//			ret += c;
+//		}
+//		else // if (to_get_from.eof())
+//		{
+//			break;
+//		}
+//	}
+//
+//	return ret;
+//}
 inline auto get_istream_as_str(std::istream&& to_get_from)
 {
 	return get_istream_as_str(to_get_from);
 }
-inline auto get_istream_as_str(std::ifstream&& to_get_from)
-{
-	return get_istream_as_str(to_get_from);
-}
+//inline auto get_istream_as_str(std::ifstream&& to_get_from)
+//{
+//	return get_istream_as_str(to_get_from);
+//}
 
 inline std::string get_stdin_as_str()
 {
