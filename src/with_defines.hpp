d8405d87ec20f9787fbb6510275dd43b2b138c1b
fl4shk <fl4shk@users.noreply.github.com>
2019-07-07 09:26:25 -0500

    stuffs

--- src/with_defines.hpp
+++ src/with_defines.hpp
@@ -1,7 +1,8 @@
 #ifndef liborangepower_with_defines_hpp
 #define liborangepower_with_defines_hpp
 
-#define with(obj, val) if (auto obj = val; true)
+#define vwith(obj, val) if (auto obj = val; true)
+#define rwith(obj, val) if (auto&& obj = val; true)
 #define zwith(obj) if (obj; true)
 #define awith(obj) if (auto obj; true)
 
