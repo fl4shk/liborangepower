110ffa63ec83460a0af6a03bfc871b5a4b5ed486
fl4shk <fl4shk@users.noreply.github.com>
2019-07-19 16:04:42 -0500

    stuffs

--- src/parser_base_class.hpp
+++ src/parser_base_class.hpp
@@ -406,40 +406,40 @@ public:		// types
 		}
 
 		template<typename FirstArgType, typename... RemArgTypes>
-		static inline TheSeqParse _opt_seq_parse(FirstArgType&& first_arg,
-			RemArgTypes&&... rem_args)
+		static inline TheSeqParse _opt_seq_parse(DerivedType* s_self,
+			FirstArgType&& first_arg, RemArgTypes&&... rem_args)
 		{
 			typename TheSeqParse::Vec s_vec;
 			_inner_seq_parse(s_vec, first_arg, rem_args...);
 
-			return TheSeqParse(std::move(s_vec), true);
+			return TheSeqParse(s_self, std::move(s_vec), true);
 		}
 		template<typename FirstArgType, typename... RemArgTypes>
-		static inline TheSeqParse _req_seq_parse(FirstArgType&& first_arg,
-			RemArgTypes&&... rem_args)
+		static inline TheSeqParse _req_seq_parse(DerivedType* s_self,
+			FirstArgType&& first_arg, RemArgTypes&&... rem_args)
 		{
 			typename TheSeqParse::Vec s_vec;
 			_inner_seq_parse(s_vec, first_arg, rem_args...);
 
-			return TheSeqParse(std::move(s_vec), false);
+			return TheSeqParse(s_self, std::move(s_vec), false);
 		}
 		template<typename FirstArgType, typename... RemArgTypes>
-		static inline TheOrParse _opt_or_parse(FirstArgType&& first_arg,
-			RemArgTypes&&... rem_args)
+		static inline TheOrParse _opt_or_parse(DerivedType* s_self,
+			FirstArgType&& first_arg, RemArgTypes&&... rem_args)
 		{
 			typename TheOrParse::Vec s_vec;
 			_inner_seq_parse(s_vec, first_arg, rem_args...);
 
-			return TheOrParse(std::move(s_vec), true);
+			return TheOrParse(s_self, std::move(s_vec), true);
 		}
 		template<typename FirstArgType, typename... RemArgTypes>
-		static inline TheOrParse _req_or_parse(FirstArgType&& first_arg,
-			RemArgTypes&&... rem_args)
+		static inline TheOrParse _req_or_parse(DerivedType* s_self,
+			FirstArgType&& first_arg, RemArgTypes&&... rem_args)
 		{
 			typename TheOrParse::Vec s_vec;
 			_inner_seq_parse(s_vec, first_arg, rem_args...);
 
-			return TheOrParse(std::move(s_vec), false);
+			return TheOrParse(s_self, std::move(s_vec), false);
 		}
 	};
 
