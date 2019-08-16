bda9231477348ae20910ddcab632c1fd586763cf
fl4shk <fl4shk@users.noreply.github.com>
2019-08-16 18:24:47 -0500

    update ParseTreeBase

--- src/parse_tree_base_class.hpp
+++ src/parse_tree_base_class.hpp
@@ -18,7 +18,7 @@ template<typename SrcCodeChunkType, typename TokType>
 class ParseTreeBase
 {
 public:		// types
-	using Child = std::unique_ptr<ParseTree>;
+	using Child = std::unique_ptr<ParseTreeBase>;
 	using DataElem = variant<bool, std::string, bignum::BigNum, TokType>;
 
 private:		// variables
@@ -28,12 +28,12 @@ private:		// variables
 	std::map<std::string, Child> _children;
 
 public:		// functions
-	inline ParseTree(const SrcCodeChunk& s_src_code_chunk)
+	inline ParseTreeBase(const SrcCodeChunk& s_src_code_chunk)
 		: _src_code_chunk(s_src_code_chunk)
 	{
 	}
-	GEN_CM_BOTH_CONSTRUCTORS_AND_ASSIGN(ParseTree);
-	inline ~ParseTree() = default;
+	GEN_CM_BOTH_CONSTRUCTORS_AND_ASSIGN(ParseTreeBase);
+	virtual inline ~ParseTreeBase() = default;
 
 	GEN_GETTER_BY_CON_REF(src_code_chunk)
 	GEN_GETTER_AND_SETTER_BY_CON_REF(ident)
