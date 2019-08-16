dc187d250029835c59a8b160393cf89691971ef2
fl4shk <fl4shk@users.noreply.github.com>
2019-08-16 17:46:30 -0500

    stuffs

---
+++ src/parse_tree_base_class.hpp
@@ -0,0 +1,48 @@
+#ifndef liborangepower_parse_tree_base_class_hpp
+#define liborangepower_parse_tree_base_class_hpp
+
+#include "gmp_stuff.hpp"
+
+
+#include <string>
+#include <map>
+#include <memory>
+
+namespace liborangepower
+{
+
+namespace lang
+{
+
+template<typename SrcCodeChunkType, typename TokType>
+class ParseTreeBase
+{
+public:		// types
+	using Child = std::unique_ptr<ParseTree>;
+	using DataElem = variant<bool, std::string, bignum::BigNum, TokType>;
+
+private:		// variables
+	SrcCodeChunkType _src_code_chunk;
+	std::string _ident;
+	std::map<std::string, DataElem> _data;
+	std::map<std::string, Child> _children;
+
+public:		// functions
+	inline ParseTree(const SrcCodeChunk& s_src_code_chunk)
+		: _src_code_chunk(s_src_code_chunk)
+	{
+	}
+	GEN_CM_BOTH_CONSTRUCTORS_AND_ASSIGN(ParseTree);
+	inline ~ParseTree() = default;
+
+	GEN_GETTER_BY_CON_REF(src_code_chunk)
+	GEN_GETTER_AND_SETTER_BY_CON_REF(ident)
+	GEN_GETTERS_BY_CON_REF_AND_REF(data)
+	GEN_GETTERS_BY_CON_REF_AND_REF(children)
+};
+
+} // namespace lang
+
+} // namespace liborangepower
+
+#endif		// liborangepower_parse_tree_base_class_hpp
