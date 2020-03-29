8c7f8a5dcd6de330be6bd800b89f5dd5725af9c0
fl4shk <fl4shk@users.noreply.github.com>
2020-03-29 12:25:37 -0500

    stuffs

--- src/linked_list_classes.hpp
+++ src/linked_list_classes.hpp
@@ -457,11 +457,11 @@ public:		// functions
 		return (head()._next == HEAD_INDEX);
 	}
 
-	inline NodeIterator begin()
+	inline NodeIterator begin() const
 	{
 		return NodeIterator(this, head().next());
 	}
-	inline NodeIterator end()
+	inline NodeIterator end() const
 	{
 		return NodeIterator(this, HEAD_INDEX);
 	}
