7539a7e0d5e623b1c0a00882bd53a65c5ce83a77
fl4shk <fl4shk@users.noreply.github.com>
2020-06-03 11:46:30 -0500

    stuffs

--- src/defer_define.hpp
+++ src/defer_define.hpp
@@ -4,17 +4,12 @@
 #include <memory>
 #include <functional>
 
-#include "cpp_magic.hpp"
-
-
-#define _INNER_DEFER(item) \
-	item
 
 #define defer(name, ...) \
 	std::shared_ptr<void> defer_ ## name (nullptr, \
 		std::bind([&]() -> void \
 		{ \
-			EVAL(MAP(_INNER_DEFER, SEMICOLON, __VA_ARGS__)) \
+			__VA_ARGS__ \
 		}))
 
 
