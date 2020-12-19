a133b8f82570fc70346f374f8c2ca1f2d9aad75f
fl4shk <fl4shk@users.noreply.github.com>
2020-12-19 13:49:58 -0600

    add in casting for u64 and i64 shifts

--- src/misc/byte_vec_accessors.hpp
+++ src/misc/byte_vec_accessors.hpp
@@ -149,27 +149,27 @@ inline void byte_vec_push_back_i32_le(std::vector<u8>& byte_vec, i32 n_val)
 inline u64 byte_vec_get_u64_le(const std::vector<u8>& byte_vec,
 	size_t index)
 {
-	const u64 ret = ((byte_vec.at(index + 7) << 56)
-		| (byte_vec.at(index + 6) << 48)
-		| (byte_vec.at(index + 5) << 40)
-		| (byte_vec.at(index + 4) << 32)
-		| (byte_vec.at(index + 3) << 24)
-		| (byte_vec.at(index + 2) << 16)
-		| (byte_vec.at(index + 1) << 8) 
-		| byte_vec.at(index + 0));
+	const u64 ret = ((byte_vec.at(index + 7) << static_cast<u64>(56))
+		| (byte_vec.at(index + 6) << static_cast<u64>(48))
+		| (byte_vec.at(index + 5) << static_cast<u64>(40))
+		| (byte_vec.at(index + 4) << static_cast<u64>(32))
+		| (byte_vec.at(index + 3) << static_cast<u64>(24))
+		| (byte_vec.at(index + 2) << static_cast<u64>(16))
+		| (byte_vec.at(index + 1) << static_cast<u64>(8))
+		| (byte_vec.at(index + 0) << static_cast<u64>(0)));
 	return ret;
 }
 inline void byte_vec_set_u64_le(std::vector<u8>& byte_vec, size_t index,
 	u64 n_val)
 {
-	byte_vec.at(index + 0) = ((n_val >> 0) & 0xff);
-	byte_vec.at(index + 1) = ((n_val >> 8) & 0xff);
-	byte_vec.at(index + 2) = ((n_val >> 16) & 0xff);
-	byte_vec.at(index + 3) = ((n_val >> 24) & 0xff);
-	byte_vec.at(index + 4) = ((n_val >> 32) & 0xff);
-	byte_vec.at(index + 5) = ((n_val >> 40) & 0xff);
-	byte_vec.at(index + 6) = ((n_val >> 48) & 0xff);
-	byte_vec.at(index + 7) = ((n_val >> 56) & 0xff);
+	byte_vec.at(index + 0) = ((n_val >> static_cast<u64>(0)) & 0xff);
+	byte_vec.at(index + 1) = ((n_val >> static_cast<u64>(8)) & 0xff);
+	byte_vec.at(index + 2) = ((n_val >> static_cast<u64>(16)) & 0xff);
+	byte_vec.at(index + 3) = ((n_val >> static_cast<u64>(24)) & 0xff);
+	byte_vec.at(index + 4) = ((n_val >> static_cast<u64>(32)) & 0xff);
+	byte_vec.at(index + 5) = ((n_val >> static_cast<u64>(40)) & 0xff);
+	byte_vec.at(index + 6) = ((n_val >> static_cast<u64>(48)) & 0xff);
+	byte_vec.at(index + 7) = ((n_val >> static_cast<u64>(56)) & 0xff);
 }
 inline void byte_vec_push_back_u64_le(std::vector<u8>& byte_vec, u64 n_val)
 {
@@ -184,27 +184,27 @@ inline void byte_vec_push_back_u64_le(std::vector<u8>& byte_vec, u64 n_val)
 inline i64 byte_vec_get_i64_le(const std::vector<u8>& byte_vec,
 	size_t index)
 {
-	const i64 ret = ((byte_vec.at(index + 7) << 56)
-		| (byte_vec.at(index + 6) << 48)
-		| (byte_vec.at(index + 5) << 40)
-		| (byte_vec.at(index + 4) << 32)
-		| (byte_vec.at(index + 3) << 24)
-		| (byte_vec.at(index + 2) << 16)
-		| (byte_vec.at(index + 1) << 8) 
-		| byte_vec.at(index + 0));
+	const i64 ret = ((byte_vec.at(index + 7) << static_cast<i64>(56))
+		| (byte_vec.at(index + 6) << static_cast<i64>(48))
+		| (byte_vec.at(index + 5) << static_cast<i64>(40))
+		| (byte_vec.at(index + 4) << static_cast<i64>(32))
+		| (byte_vec.at(index + 3) << static_cast<i64>(24))
+		| (byte_vec.at(index + 2) << static_cast<i64>(16))
+		| (byte_vec.at(index + 1) << static_cast<i64>(8))
+		| (byte_vec.at(index + 0) << static_cast<i64>(0)));
 	return ret;
 }
 inline void byte_vec_set_i64_le(std::vector<u8>& byte_vec, size_t index,
 	i64 n_val)
 {
-	byte_vec.at(index + 0) = ((n_val >> 0) & 0xff);
-	byte_vec.at(index + 1) = ((n_val >> 8) & 0xff);
-	byte_vec.at(index + 2) = ((n_val >> 16) & 0xff);
-	byte_vec.at(index + 3) = ((n_val >> 24) & 0xff);
-	byte_vec.at(index + 4) = ((n_val >> 32) & 0xff);
-	byte_vec.at(index + 5) = ((n_val >> 40) & 0xff);
-	byte_vec.at(index + 6) = ((n_val >> 48) & 0xff);
-	byte_vec.at(index + 7) = ((n_val >> 56) & 0xff);
+	byte_vec.at(index + 0) = ((n_val >> static_cast<i64>(0)) & 0xff);
+	byte_vec.at(index + 1) = ((n_val >> static_cast<i64>(8)) & 0xff);
+	byte_vec.at(index + 2) = ((n_val >> static_cast<i64>(16)) & 0xff);
+	byte_vec.at(index + 3) = ((n_val >> static_cast<i64>(24)) & 0xff);
+	byte_vec.at(index + 4) = ((n_val >> static_cast<i64>(32)) & 0xff);
+	byte_vec.at(index + 5) = ((n_val >> static_cast<i64>(40)) & 0xff);
+	byte_vec.at(index + 6) = ((n_val >> static_cast<i64>(48)) & 0xff);
+	byte_vec.at(index + 7) = ((n_val >> static_cast<i64>(56)) & 0xff);
 }
 inline void byte_vec_push_back_i64_le(std::vector<u8>& byte_vec, i64 n_val)
 {
