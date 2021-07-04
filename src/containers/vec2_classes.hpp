0c880879d2288b1070400a4f94fec99eadbd00ce
fl4shk <fl4shk@users.noreply.github.com>
2021-07-04 14:23:49 -0500

    `src/containers/vec2_classes.hpp`: add math operations to `Vec2` `src/containers/vec3_classes.hpp`: add math operations to `Vec3`

--- src/containers/vec2_classes.hpp
+++ src/containers/vec2_classes.hpp
@@ -3,6 +3,8 @@
 
 #include "../gen_class_innards_defines.hpp"
 
+#include <cmath>
+
 namespace liborangepower
 {
 
@@ -16,6 +18,7 @@ public:		// variables
 	Type x, y;
 
 public:		// functions
+	//--------
 	inline Vec2() = default;
 	inline Vec2(const Type& s_x, const Type& s_y)
 		: x(s_x), y(s_y)
@@ -24,7 +27,112 @@ public:		// functions
 
 	GEN_CM_BOTH_CONSTRUCTORS_AND_ASSIGN(Vec2);
 	virtual ~Vec2() = default;
+	//--------
+	template<typename OtherType>
+	inline Vec2 operator + (const Vec2<OtherType>& other) const
+	{
+		return Vec2<Type>(x + other.x, y + other.y);
+	}
+	template<typename OtherType>
+	inline Vec2& operator += (const Vec2<OtherType>& other) const
+	{
+		*this = (*this) + other;
+		return *this;
+	}
 
+	template<typename OtherType>
+	inline Vec2 operator - (const Vec2<OtherType>& other) const
+	{
+		return Vec2<Type>(x - other.x, y - other.y);
+	}
+	template<typename OtherType>
+	inline Vec2& operator -= (const Vec2<OtherType>& other) const
+	{
+		*this = (*this) - other;
+		return *this;
+	}
+
+	inline Vec2 operator - () const
+	{
+		return Vec2(-x, -y);
+	}
+
+	template<typename OtherType>
+	inline Vec2 operator * (const OtherType& scale) const
+	{
+		return Vec2<Type>(x * scale, y * scale);
+	}
+	template<typename OtherType>
+	inline Vec2& operator *= (const OtherType& other) const
+	{
+		*this = (*this) * other;
+		return *this;
+	}
+
+	template<typename OtherType>
+	inline Vec2 operator / (const OtherType& scale) const
+	{
+		return Vec2<Type>(x / scale, y / scale);
+	}
+	template<typename OtherType>
+	inline Vec2& operator /= (const OtherType& scale) const
+	{
+		*this = (*this) / scale;
+		return *this;
+	}
+	//--------
+	// Dot product
+	template<typename OtherType>
+	inline Type dot_prod(const Vec2<OtherType>& other) const
+	{
+		return Type((x * other.x) + (y * other.y));
+	}
+
+	// Z component of a 3D cross product, which is computed as if *this and
+	// to_zcross have been converted to 3D vectors with Z components of zero
+	template<typename OtherType>
+	inline Type zcross_prod(const Vec2<OtherType>& other) const
+	{
+		return Type((x * other.y) - (y * other.x));
+	}
+
+	inline Type norm() const
+	{
+		return std::sqrt(dot_prod(*this));
+	}
+	//--------
+	template<typename OtherType>
+	inline bool operator == (const Vec2<OtherType>& other) const
+	{
+		return ((x == other.x) && (y == other.y));
+	}
+	template<typename OtherType>
+	inline bool operator != (const Vec2<OtherType>& other) const
+	{
+		return (!((*this) == other));
+	}
+
+	template<typename OtherType>
+	inline bool operator < (const Vec2<OtherType>& other) const
+	{
+		return ((y < other.y) || ((y == other.y) && (x < other.x)));
+	}
+	template<typename OtherType>
+	inline bool operator > (const Vec2<OtherType>& other) const
+	{
+		return ((y > other.y) || ((y == other.y) && (x > other.x)));
+	}
+	template<typename OtherType>
+	inline bool operator <= (const Vec2<OtherType>& other) const
+	{
+		return (!((*this) > other));
+	}
+	template<typename OtherType>
+	inline bool operator >= (const Vec2<OtherType>& other) const
+	{
+		return (!((*this) < other));
+	}
+	//--------
 };
 
 } // namespace containers
--- src/containers/vec3_classes.hpp
+++ src/containers/vec3_classes.hpp
@@ -1,6 +1,9 @@
 #ifndef liborangepower_containers_vec3_classes_hpp
 #define liborangepower_containers_vec3_classes_hpp
 
+#include "../gen_class_innards_defines.hpp"
+#include "vec2_classes.hpp"
+
 namespace liborangepower
 {
 
@@ -11,15 +14,126 @@ template<typename Type>
 class Vec3
 {
 public:		// variables
-	Type x = 0, y = 0, z = 0;
+	Type x, y, z;
 public:		// functions
+	//--------
 	inline Vec3() = default;
 	inline Vec3(const Type& s_x, const Type& s_y, const Type& s_z)
 		: x(s_x), y(s_y), z(s_z)
 	{
 	}
+
 	GEN_CM_BOTH_CONSTRUCTORS_AND_ASSIGN(Vec3);
 	virtual inline ~Vec3() = default;
+	//--------
+	template<typename OtherType>
+	inline Vec3 operator + (const Vec3<OtherType>& other) const
+	{
+		return Vec3<Type>(x + other.x, y + other.y, z + other.z);
+	}
+	template<typename OtherType>
+	inline Vec3& operator += (const Vec3<OtherType>& other) const
+	{
+		*this = (*this) + other;
+		return *this;
+	}
+
+	template<typename OtherType>
+	inline Vec3 operator - (const Vec3<OtherType>& other) const
+	{
+		return Vec3<Type>(x - other.x, y - other.y, z - other.z);
+	}
+	template<typename OtherType>
+	inline Vec3& operator -= (const Vec3<OtherType>& other) const
+	{
+		*this = (*this) - other;
+		return *this;
+	}
+
+	inline Vec3 operator - () const
+	{
+		return Vec3(-x, -y, -z);
+	}
+
+	template<typename OtherType>
+	inline Vec3 operator * (const OtherType& scale) const
+	{
+		return Vec3(x * scale, y * scale, z * scale);
+	}
+	template<typename OtherType>
+	inline Vec3& operator *= (const OtherType& other) const
+	{
+		*this = (*this) * other;
+		return *this;
+	}
+
+	template<typename OtherType>
+	inline Vec3 operator / (const OtherType& scale) const
+	{
+		return Vec3<Type>(x / scale, y / scale, z / scale);
+	}
+	template<typename OtherType>
+	inline Vec3& operator /= (const OtherType& scale) const
+	{
+		*this = (*this) / scale;
+		return *this;
+	}
+	//--------
+	// Dot product
+	template<typename OtherType>
+	inline Type dot_prod(const Vec3<OtherType>& other) const
+	{
+		return ((x * other.x) + (y * other.y) + (z * other.z));
+	}
+
+	// Cross product
+	template<typename OtherType>
+	inline Vec3 cross_prod(const Vec3<OtherType>& other) const
+	{
+		return Vec3<Type>((y * other.z) - (z * other.y),
+			(z * other.x) - (x * other.z),
+			(x * other.y) - (y * other.x));
+	}
+
+	inline Type norm() const
+	{
+		return std::sqrt(dot_prod(*this));
+	}
+	//--------
+	template<typename OtherType>
+	inline bool operator == (const Vec3<OtherType>& other) const
+	{
+		return ((x == other.x) && (y == other.y) && (z == other.z));
+	}
+	template<typename OtherType>
+	inline bool operator != (const Vec3<OtherType>& other) const
+	{
+		return (!((*this) == other));
+	}
+
+	template<typename OtherType>
+	inline bool operator < (const Vec3<OtherType>& other) const
+	{
+		return ((z < other.z)
+			|| ((z == other.z) && (Vec2(x, y) < Vec2(other.x, other.y))));
+	}
+	template<typename OtherType>
+	inline bool operator > (const Vec3<OtherType>& other) const
+	{
+		return ((z > other.z)
+			|| ((z == other.z) && (Vec2(x, y) > Vec2(other.x, other.y))));
+	}
+	template<typename OtherType>
+	inline bool operator <= (const Vec3<OtherType>& other) const
+	{
+		return (!((*this) > other));
+	}
+	template<typename OtherType>
+	inline bool operator >= (const Vec3<OtherType>& other) const
+	{
+		return (!((*this) < other));
+	}
+	//--------
 };
 
 } // namespace containers
