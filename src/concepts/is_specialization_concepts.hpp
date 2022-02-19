685caf61553805f26dd07fb5a290349b62a64429
fl4shk <fl4shk@users.noreply.github.com>
2022-02-19 14:54:23 -0600

    `src/concepts/is_specialization_concepts.hpp`: working towards new version of this

--- src/concepts/is_specialization_concepts.hpp
+++ src/concepts/is_specialization_concepts.hpp
@@ -8,13 +8,66 @@ namespace liborangepower
 namespace misc_util
 {
 
-template<typename, template<typename...> typename>
-constexpr inline bool is_specialization = false;
-template<template<typename...> typename Type, typename... Args>
-constexpr inline bool is_specialization<Type<Args...>, Type> = true;
+//template<typename, template<typename...> typename>
+//constexpr inline bool is_specialization = false;
+//template<template<typename...> typename Type, typename... Args>
+//constexpr inline bool is_specialization<Type<Args...>, Type> = true;
+
+//template<typename Type, template<typename...> typename NonSpecType>
+//constexpr inline bool is_specialization = false;
+
+//template<typename Type>
+//extern uint32_t _is_move_only_prev_curr_pair_func
+//	(const MoveOnlyPrevCurrPair<Type>&);
+//template<typename Type>
+//extern uint8_t _is_move_only_prev_curr_pair_func(const Type&);
+//
+//template<typename Type>
+//constexpr inline bool is_move_only_prev_curr_pair()
+//{
+//	return (sizeof(_is_move_only_prev_curr_pair_func(std::declval<Type>()))
+//		== sizeof(uint32_t));
+//}
+//template<typename FirstArgType,
+//	template<typename...> typename ContainerEtcType,
+//	typename... RemArgTypes>
+//extern uint32_t is_specialization
+//	(const ContainerEtcType<FirstArgType, RemArgTypes...>&);
+//template<typename Type,
+//	template<typename...> typename ContainerEtcType,
+//	typename... RemArgTypes>
+//extern uint8_t is_specialization(const Type&);
+//
+//template<typename Type, template<typename...> typename ContainerEtcType,
+//	typename... RemArgTypes>
+//constexpr inline bool is_specialization 
+//	= (sizeof(is_specialization<Type, ContainerEtcType, RemArgTypes...>
+//		(std::declval<Type>()))
+//		== sizeof(uint32_t));
+
+template<typename FirstArgType,
+	template<typename...> typename ContainerEtcType,
+	typename RemArgTypes...>
+extern uint8_t is_specialization(const FirstArgType&);
+template<typename FirstArgType,
+	template<typename...> typename ContainerEtcType,
+	typename RemArgTypes...>
+extern uint32_t is_specialization
+	(const ContainerEtcType<FirstArgType, RemArgTypes...>&);
+
+template<typename FirstArgType,
+	template<typename...> typename ContainerEtcType,
+	typename RemArgTypes...>
+constexpr inline bool is_specialization()
+{
+    return (sizeof(is_specialization
+		<FirstArgType, ContainerEtcType, RemArgTypes...>
+		(std::declval<Type>()))
+		== sizeof(uint32_t));
+}
 
 template<typename Type, template<typename...> typename NonSpecType>
-concept IsSpecialization = is_specialization<Type, NonSpecType>;
+concept IsSpecialization = is_specialization<Type, NonSpecType>();
 
 } // namespace misc_util
 } // namespace liborangepower
