fd53147162afa48490b444be3797a46f7619f92c
fl4shk <fl4shk@users.noreply.github.com>
2022-02-20 14:57:25 -0600

    potentially finally fix it!

--- src/concepts/is_specialization_concepts.hpp
+++ src/concepts/is_specialization_concepts.hpp
@@ -36,7 +36,7 @@ namespace concepts
 		== sizeof(uint32_t))
 //--------
 template<template<typename...> typename ContainerEtcType,
-	typename FirstArgType, typename... RemArgTypes>
+	typename FirstArgType>
 extern uint8_t _is_specialization_check(const FirstArgType&);
 
 template<template<typename...> typename ContainerEtcType,
@@ -53,23 +53,23 @@ constexpr inline bool is_specialization()
 		== sizeof(uint32_t));
 }
 //--------
-//template<template<typename, auto...> typename ContainerEtcType,
-//	typename ArgType, size_t... RemArgs>
-//extern uint8_t _is_specialization_rtargs_check(const ArgType&);
-//
-//template<template<typename...> typename ContainerEtcType,
-//	typename ArgType, size_t... RemArgs>
-//extern uint32_t _is_specialization_rtargs_check
-//	(const ContainerEtcType<ArgType, RemArgs...>&);
-//
-//template<typename ToCheckType,
-//	template<typename, size_t...> typename ContainerEtcType>
-//constexpr inline bool is_specialization_rtargs()
-//{
-//	return (sizeof(_is_specialization_rtargs_check<ContainerEtcType>
-//		(std::declval<ToCheckType>()))
-//		== sizeof(uint32_t));
-//}
+template<template<typename, auto...> typename ContainerEtcType,
+	typename ArgType>
+extern uint8_t _is_specialization_check(const ArgType&);
+
+template<template<typename, auto...> typename ContainerEtcType,
+	typename ArgType, auto... RemArgs>
+extern uint32_t _is_specialization_check
+	(const ContainerEtcType<ArgType, RemArgs...>&);
+
+template<typename ToCheckType,
+	template<typename, auto...> typename ContainerEtcType>
+constexpr inline bool is_specialization()
+{
+	return (sizeof(_is_specialization_check<ContainerEtcType>
+		(std::declval<ToCheckType>()))
+		== sizeof(uint32_t));
+}
 //--------
 template<typename Type, template<typename...> typename ContainerEtcType>
 concept IsSpecialization = is_specialization<Type, ContainerEtcType>();
