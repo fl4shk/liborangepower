ae2998dedb7405e25bac9e9b674a84c57769b270
fl4shk <fl4shk@users.noreply.github.com>
2022-02-26 17:08:09 -0600

    `json::get_jv_memb_w_stat_cast()`: swap order of template arguments

--- src/json_stuff/json_stuff.hpp
+++ src/json_stuff/json_stuff.hpp
@@ -248,7 +248,7 @@ inline void get_jv_memb(auto& ret, const Json::Value& jv,
 {
 	val_from_jv<decltype(ret)>(ret, jv[name]);
 }
-template<typename RetT, typename TempT>
+template<typename TempT, typename RetT>
 inline void get_jv_memb_w_stat_cast(RetT& ret, const Json::Value& jv,
 	const std::string& name)
 {
