e114ee1a0fd968384f075758d4eb8ead80e3f3da
fl4shk <fl4shk@users.noreply.github.com>
2022-04-17 14:29:34 -0500

    `src/gfx/plot_stuff.hpp`: `#include "../containers/vec2_classes.hpp"`

--- src/gfx/plot_stuff.hpp
+++ src/gfx/plot_stuff.hpp
@@ -5,12 +5,15 @@
 
 #include "../misc/misc_includes.hpp"
 #include "../misc/misc_types.hpp"
+#include "../containers/vec2_classes.hpp"
 
 #include <cstdlib>
 #include <concepts>
 
 namespace liborangepower
 {
+
+using containers::Vec2;
 
 namespace gfx
 {
@@ -69,7 +72,6 @@ std::vector<Vec2<i32>> bresenham(const Vec2<i32>& p0, const Vec2<i32>& p1)
 }
 
 } // namespace gfx
-
 } // namespace liborangepower
 
 #endif		// liborangepower_gfx_plot_stuff_hpp
