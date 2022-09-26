8e98751bf4d9ca11d11b3ee96727be3e55e6b217
fl4shk <fl4shk@users.noreply.github.com>
2022-09-26 11:59:43 -0500

    `src/time/time_stuff.hpp`: comment out `Prng`. rework `rng_run()` and friends

--- src/time/time_stuff.hpp
+++ src/time/time_stuff.hpp
@@ -69,136 +69,149 @@ concept CallableLikeRng = requires(RngT rng)
 {
 	{ rng() } -> std::convertible_to<T>;
 };
+template<typename RngT, typename T>
+concept CallableLikeRngBounded = requires(RngT rng, T bound)
+{
+	{ rng(bound) } -> std::convertible_to<T>;
+};
 
 template<typename T, CallableLikeRng<T> RngT>
 inline auto rng_run(RngT& rng)
 {
 	return T(rng());
 }
-template<typename T, CallableLikeRng<T> RngT>
-inline auto rng_run(RngT& rng, const T& max, bool saturate=false)
+//template<typename T, CallableLikeRng<T> RngT>
+//inline auto rng_run(RngT& rng, const T& max, bool saturate=false)
+//{
+//	T ret = rng_run<T>(rng);
+//
+//	if (!saturate)
+//	{
+//		ret %= max;
+//	}
+//	else // if (saturate)
+//	{
+//		if (ret > max)
+//		{
+//			ret = max;
+//		}
+//	}
+//
+//	return ret;
+//}
+template<typename T, CallableLikeRngBounded<T> RngT>
+inline auto rng_run(RngT& rng, const T& bound)
 {
-	T ret = rng_run<T>(rng);
-
-	if (!saturate)
-	{
-		ret %= max;
-	}
-	else // if (saturate)
-	{
-		if (ret > max)
-		{
-			ret = max;
-		}
-	}
-
-	return ret;
+	return T(rng(bound));
 }
-template<typename T, CallableLikeRng<T> RngT>
-inline auto rng_run_lim(RngT& rng, const T& lim_0, const T& lim_1)
+
+template<typename T, CallableLikeRngBounded<T> RngT>
+inline auto rng_run(RngT& rng, const T& lim_0, const T& lim_1)
 {
-	return rng_run<T>(rng, math::cstm_abs(lim_0 - lim_1), false)
+	//return rng_run<T>(rng, math::cstm_abs(lim_1 - lim_0), false)
+	//	+ math::min_va(lim_0, lim_1);
+	return rng_run<T>(rng, math::cstm_abs(lim_1 - lim_0))
 		+ math::min_va(lim_0, lim_1);
 }
 
-template<typename T, CallableLikeRng<T> RngT>
-inline auto rng_run_scaled(RngT& rng, const T& scale)
-{
-	return rng_run<T>(rng) * scale;
-}
-template<typename T, CallableLikeRng<T> RngT>
-inline auto rng_run_scaled(RngT& rng, const T& scale, const T& max,
-	bool saturate=false)
-{
-	return rng_run<T>(rng, max, saturate) * scale;
-}
-template<typename T, CallableLikeRng<T> RngT>
-inline auto rng_run_scaled_lim(RngT& rng, const T& scale, const T& lim_0,
-	const T& lim_1)
-{
-	return rng_run_lim<T>(rng, lim_0, lim_1) * scale;
-}
+//template<typename T, CallableLikeRngBounded<T> RngT>
+//inline auto rng_run_scaled(RngT& rng, const T& bound, const T& scale)
+//{
+//	return rng_run_bounded<T>(rng, bound) * scale;
+//}
+//template<typename T, CallableLikeRng<T> RngT>
+//inline auto rng_run_scaled(RngT& rng, const T& lim_0, const T& lim_1,
+//	const T& scale)
+//{
+//	return rng_run_lim<T>(rng, max, saturate) * scale;
+//}
+//template<typename T, CallableLikeRng<T> RngT>
+//inline auto rng_run_scaled_lim(RngT& rng, const T& lim_0, const T& lim_1,
+//	const T& scale)
+//{
+//	return rng_run_lim<T>(rng, lim_0, lim_1) * scale;
+//}
 
-template<typename _InstanceT=std::mt19937_64>
-class Prng final
-{
-public:		// types
-	using SeedT = decltype(get_hrc_now_rng_seed());
-
-	//using InstanceT = std::minstd_rand;
-	//using InstanceT = std::mt19937;
-	//using InstanceT = std::mt19937_64;
-	using InstanceT = _InstanceT;
-private:		// variables
-	//i64 _param_0, _param_1;
-
-	InstanceT _instance;
-private:		// functions
-	inline SeedT _default_initial_seed(i64 param_0, i64 param_1)
-	{
-		// I have no idea how good this is, but it seems to work?
-		return (get_hrc_now_rng_seed() * (param_0 + i64(1))) + param_1;
-	}
-public:		// functions
-	inline Prng(i64 param_0=0, i64 param_1=0)
-		: _instance(_default_initial_seed(param_0, param_1))
-	{
-	}
-	inline Prng(SeedT s_seed)
-		: _instance(s_seed)
-	{
-	}
-	GEN_CM_BOTH_CONSTRUCTORS_AND_ASSIGN(Prng);
-	inline ~Prng() = default;
-
-	inline auto operator () ()
-	{
-		return rng_run<decltype(_instance())>(_instance);
-	}
-	template<typename T>
-	inline auto operator () (const T& max, bool saturate=false)
-	{
-		return rng_run<T>(_instance, max, saturate);
-	}
-	template<typename T>
-	inline auto run()
-	{
-		return rng_run<T>(_instance);
-	}
-	template<typename T>
-	inline auto run_scale(const T& scale)
-	{
-		return rng_run_scaled<T>(_instance, scale);
-	}
-	template<typename T>
-	inline auto run_scaled(const T& scale, const T& max,
-		bool saturate=false)
-	{
-		return rng_run_scaled<T>(_instance, scale, max, saturate);
-	}
-
-
-
-	// For serialization
-	template<typename CharT, typename Traits=std::char_traits<CharT>>
-	inline std::basic_ostream<CharT, Traits>& operator <<
-		(std::basic_ostream<CharT, Traits>& os) const
-	{
-		os << _instance;
-		return os;
-	}
-	template<typename CharT, typename Traits=std::char_traits<CharT>>
-	inline std::basic_istream<CharT, Traits>& operator >>
-		(std::basic_istream<CharT, Traits>& is)
-	{
-		is >> _instance;
-		return is;
-	}
-
-	//GEN_GETTER_BY_VAL(param_0)
-	//GEN_GETTER_BY_VAL(param_1)
-	GEN_GETTER_BY_CON_REF(instance)
-};
+//template<typename _InstanceT=std::mt19937_64>
+//class Prng final
+//{
+//public:		// types
+//	using SeedT = decltype(get_hrc_now_rng_seed());
+//
+//	//using InstanceT = std::minstd_rand;
+//	//using InstanceT = std::mt19937;
+//	//using InstanceT = std::mt19937_64;
+//	using InstanceT = _InstanceT;
+//private:		// variables
+//	//i64 _param_0, _param_1;
+//
+//	InstanceT _instance;
+//private:		// functions
+//	inline SeedT _default_initial_seed(i64 param_0, i64 param_1)
+//	{
+//		// I have no idea how good this is, but it seems to work?
+//		return (get_hrc_now_rng_seed() * (param_0 + i64(1))) + param_1;
+//	}
+//public:		// functions
+//	inline Prng(i64 param_0=0, i64 param_1=0)
+//		: _instance(_default_initial_seed(param_0, param_1))
+//	{
+//	}
+//	inline Prng(SeedT s_seed)
+//		: _instance(s_seed)
+//	{
+//	}
+//	GEN_CM_BOTH_CONSTRUCTORS_AND_ASSIGN(Prng);
+//	inline ~Prng() = default;
+//
+//	inline auto operator () ()
+//	{
+//		return rng_run<decltype(_instance())>(_instance);
+//	}
+//	template<typename T>
+//	inline auto operator () (const T& max, bool saturate=false)
+//	{
+//		return rng_run<T>(_instance, max, saturate);
+//	}
+//	template<typename T>
+//	inline auto run()
+//	{
+//		return rng_run<T>(_instance);
+//	}
+//	template<typename T>
+//	inline auto run_scale(const T& scale)
+//	{
+//		return rng_run_scaled<T>(_instance, scale);
+//	}
+//	template<typename T>
+//	inline auto run_scaled(const T& scale, const T& max,
+//		bool saturate=false)
+//	{
+//		return rng_run_scaled<T>(_instance, scale, max, saturate);
+//	}
+//
+//
+//
+//	// For serialization
+//	template<typename CharT, typename Traits=std::char_traits<CharT>>
+//	inline std::basic_ostream<CharT, Traits>& operator <<
+//		(std::basic_ostream<CharT, Traits>& os) const
+//	{
+//		os << _instance;
+//		return os;
+//	}
+//	template<typename CharT, typename Traits=std::char_traits<CharT>>
+//	inline std::basic_istream<CharT, Traits>& operator >>
+//		(std::basic_istream<CharT, Traits>& is)
+//	{
+//		is >> _instance;
+//		return is;
+//	}
+//
+//	//GEN_GETTER_BY_VAL(param_0)
+//	//GEN_GETTER_BY_VAL(param_1)
+//	GEN_GETTER_BY_CON_REF(instance)
+//};
 //--------
 class Profiler
 {
