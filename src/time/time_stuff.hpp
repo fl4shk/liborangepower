06e3989c7f6ed0d41ff893430707aee6f441e769
fl4shk <fl4shk@users.noreply.github.com>
2022-10-09 12:59:58 -0500

    `src/time/time_stuff.hpp`: update code style

--- src/time/time_stuff.hpp
+++ src/time/time_stuff.hpp
@@ -12,87 +12,69 @@
 
 #include <pcg_random.hpp>
 
-namespace liborangepower
-{
+namespace liborangepower {
 
 using integer_types::u64;
 using integer_types::i64;
 
-namespace time
-{
+namespace time {
 //--------
-inline auto get_hrc_now()
-{
+inline auto get_hrc_now() {
 	return std::chrono::high_resolution_clock::now();
 }
-inline auto get_hrc_now_time_t()
-{
+inline auto get_hrc_now_time_t() {
 	return std::chrono::high_resolution_clock::to_time_t(get_hrc_now());
 }
-inline auto get_hrc_now_rng_seed()
-{
+inline auto get_hrc_now_rng_seed() {
 	return get_hrc_now().time_since_epoch().count();
 }
 //--------
-inline std::time_t now_as_time_t()
-{
+inline std::time_t now_as_time_t() {
 	return get_hrc_now_time_t();
 }
-inline std::tm* now_as_localtime()
-{
+inline std::tm* now_as_localtime() {
 	auto now = now_as_time_t();
 	auto ret = std::localtime(&now);
 
 	return ret;
 }
-inline std::tm* now_as_gmtime()
-{
+inline std::tm* now_as_gmtime() {
 	auto now = now_as_time_t();
 	auto ret = std::gmtime(&now);
 
 	return ret;
 }
 //--------
-inline auto put_now_as_localtime()
-{
+inline auto put_now_as_localtime() {
 	//return std::put_time(now_as_localtime(), "%c");
 	return std::put_time(now_as_localtime(), "%Y-%m-%d %H:%M:%S %Z");
 }
-inline auto put_now_as_gmtime()
-{
+inline auto put_now_as_gmtime() {
 	//return std::put_time(now_as_gmtime(), "%c");
 	return std::put_time(now_as_gmtime(), "%Y-%m-%d %H:%M:%S %Z");
 }
 //--------
 template<typename RngT, typename T>
-concept CallableLikeRng = requires(RngT rng)
-{
+concept CallableLikeRng = requires(RngT rng) {
 	{ rng() } -> std::convertible_to<T>;
 };
 template<typename RngT, typename T>
-concept CallableLikeRngBounded = requires(RngT rng, T bound)
-{
+concept CallableLikeRngBounded = requires(RngT rng, T bound) {
 	{ rng(bound) } -> std::convertible_to<T>;
 };
 
 template<typename T, CallableLikeRng<T> RngT>
-inline auto rng_run(RngT& rng)
-{
+inline auto rng_run(RngT& rng) {
 	return T(rng());
 }
 //template<typename T, CallableLikeRng<T> RngT>
-//inline auto rng_run(RngT& rng, const T& max, bool saturate=false)
-//{
+//inline auto rng_run(RngT& rng, const T& max, bool saturate=false) {
 //	T ret = rng_run<T>(rng);
 //
-//	if (!saturate)
-//	{
+//	if (!saturate) {
 //		ret %= max;
-//	}
-//	else // if (saturate)
-//	{
-//		if (ret > max)
-//		{
+//	} else { // if (saturate)
+//		if (ret > max) {
 //			ret = max;
 //		}
 //	}
@@ -100,22 +82,17 @@ inline auto rng_run(RngT& rng)
 //	return ret;
 //}
 template<typename T, CallableLikeRngBounded<T> RngT>
-inline auto rng_run_lt_bound(RngT& rng, const T& bound)
-{
+inline auto rng_run_lt_bound(RngT& rng, const T& bound) {
 	return bound == T(0) ? T(0) : T(rng(bound));
-	//if (bound == T(0))
-	//{
+	//if (bound == T(0)) {
 	//	return T(0);
-	//}
-	//else
-	//{
+	//} else {
 	//	return T(rng(bound));
 	//}
 }
 
 template<typename T, CallableLikeRngBounded<T> RngT>
-inline auto rng_run(RngT& rng, const T& lim_0, const T& lim_1)
-{
+inline auto rng_run(RngT& rng, const T& lim_0, const T& lim_1) {
 	//return rng_run<T>(rng, math::cstm_abs(lim_1 - lim_0), false)
 	//	+ math::min_va(lim_0, lim_1);
 	const auto& bound = math::cstm_abs(lim_0 - lim_1) + T(1);
@@ -124,26 +101,24 @@ inline auto rng_run(RngT& rng, const T& lim_0, const T& lim_1)
 }
 
 //template<typename T, CallableLikeRngBounded<T> RngT>
-//inline auto rng_run_scaled(RngT& rng, const T& bound, const T& scale)
-//{
+//inline auto rng_run_scaled(RngT& rng, const T& bound, const T& scale) {
 //	return rng_run_bounded<T>(rng, bound) * scale;
 //}
 //template<typename T, CallableLikeRng<T> RngT>
-//inline auto rng_run_scaled(RngT& rng, const T& lim_0, const T& lim_1,
-//	const T& scale)
-//{
+//inline auto rng_run_scaled(
+//	RngT& rng, const T& lim_0, const T& lim_1, const T& scale
+//) {
 //	return rng_run_lim<T>(rng, max, saturate) * scale;
 //}
 //template<typename T, CallableLikeRng<T> RngT>
-//inline auto rng_run_scaled_lim(RngT& rng, const T& lim_0, const T& lim_1,
-//	const T& scale)
-//{
+//inline auto rng_run_scaled_lim(
+//	RngT& rng, const T& lim_0, const T& lim_1, const T& scale
+//) {
 //	return rng_run_lim<T>(rng, lim_0, lim_1) * scale;
 //}
 
 //template<typename TargInstanceT=std::mt19937_64>
-//class Prng final
-//{
+//class Prng final {
 //public:		// types
 //	using SeedT = decltype(get_hrc_now_rng_seed());
 //
@@ -156,46 +131,39 @@ inline auto rng_run(RngT& rng, const T& lim_0, const T& lim_1)
 //
 //	InstanceT _instance;
 //private:		// functions
-//	inline SeedT _default_initial_seed(i64 param_0, i64 param_1)
-//	{
+//	inline SeedT _default_initial_seed(i64 param_0, i64 param_1) {
 //		// I have no idea how good this is, but it seems to work?
 //		return (get_hrc_now_rng_seed() * (param_0 + i64(1))) + param_1;
 //	}
 //public:		// functions
 //	inline Prng(i64 param_0=0, i64 param_1=0)
-//		: _instance(_default_initial_seed(param_0, param_1))
-//	{
+//		: _instance(_default_initial_seed(param_0, param_1)) {
 //	}
 //	inline Prng(SeedT s_seed)
-//		: _instance(s_seed)
-//	{
+//		: _instance(s_seed) {
 //	}
 //	GEN_CM_BOTH_CONSTRUCTORS_AND_ASSIGN(Prng);
 //	inline ~Prng() = default;
 //
-//	inline auto operator () ()
-//	{
+//	inline auto operator () () {
 //		return rng_run<decltype(_instance())>(_instance);
 //	}
 //	template<typename T>
-//	inline auto operator () (const T& max, bool saturate=false)
-//	{
+//	inline auto operator () (const T& max, bool saturate=false) {
 //		return rng_run<T>(_instance, max, saturate);
 //	}
 //	template<typename T>
-//	inline auto run()
-//	{
+//	inline auto run() {
 //		return rng_run<T>(_instance);
 //	}
 //	template<typename T>
-//	inline auto run_scale(const T& scale)
-//	{
+//	inline auto run_scale(const T& scale) {
 //		return rng_run_scaled<T>(_instance, scale);
 //	}
 //	template<typename T>
-//	inline auto run_scaled(const T& scale, const T& max,
-//		bool saturate=false)
-//	{
+//	inline auto run_scaled(
+//		const T& scale, const T& max, bool saturate=false
+//	) {
 //		return rng_run_scaled<T>(_instance, scale, max, saturate);
 //	}
 //
@@ -203,16 +171,16 @@ inline auto rng_run(RngT& rng, const T& lim_0, const T& lim_1)
 //
 //	// For serialization
 //	template<typename CharT, typename Traits=std::char_traits<CharT>>
-//	inline std::basic_ostream<CharT, Traits>& operator <<
-//		(std::basic_ostream<CharT, Traits>& os) const
-//	{
+//	inline std::basic_ostream<CharT, Traits>& operator << (
+//		std::basic_ostream<CharT, Traits>& os
+//	) const {
 //		os << _instance;
 //		return os;
 //	}
 //	template<typename CharT, typename Traits=std::char_traits<CharT>>
-//	inline std::basic_istream<CharT, Traits>& operator >>
-//		(std::basic_istream<CharT, Traits>& is)
-//	{
+//	inline std::basic_istream<CharT, Traits>& operator >> (
+//		std::basic_istream<CharT, Traits>& is
+//	) {
 //		is >> _instance;
 //		return is;
 //	}
@@ -222,21 +190,18 @@ inline auto rng_run(RngT& rng, const T& lim_0, const T& lim_1)
 //	GEN_GETTER_BY_CON_REF(instance)
 //};
 //--------
-class Profiler
-{
+class Profiler {
 protected:		// variables
 	std::chrono::high_resolution_clock::time_point start_tp, end_tp;
 
 public:		// functions
 
 public:		// functions
-	inline void start()
-	{
+	inline void start() {
 		start_tp = get_hrc_now();
 	}
 
-	inline auto stop()
-	{
+	inline auto stop() {
 		end_tp = get_hrc_now();
 
 		std::chrono::duration<double> diff_dtn = end_tp - start_tp;
