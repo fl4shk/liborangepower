f803e7d9f75b8c9cb2d65bc2aed485ecfe88788d
fl4shk <fl4shk@users.noreply.github.com>
2022-11-01 17:10:01 -0500

    `src/misc/misc_input_classes.hpp`: update code style

--- src/misc/misc_input_classes.hpp
+++ src/misc/misc_input_classes.hpp
@@ -5,62 +5,47 @@
 #include "misc_includes.hpp"
 #include <fstream>
 
-namespace liborangepower
-{
-namespace misc_input
-{
-
-inline std::string get_istream_as_str(std::istream& to_get_from)
-{
+namespace liborangepower {
+namespace misc_input {
+//--------
+inline std::string get_istream_as_str(std::istream& to_get_from) {
 	std::string ret;
 
 	int c;
-	for (;;)
-	{
+	for (;;) {
 		c = to_get_from.get();
-		if (!to_get_from.eof())
-		{
+		if (!to_get_from.eof()) {
 			ret += c;
-		}
-		else // if (to_get_from.eof())
-		{
+		} else { // if (to_get_from.eof())
 			break;
 		}
 	}
 
 	return ret;
 }
-//inline std::string get_istream_as_str(std::ifstream& to_get_from)
-//{
+//inline std::string get_istream_as_str(std::ifstream& to_get_from) {
 //	std::string ret;
 //
 //	int c;
-//	for (;;)
-//	{
+//	for (;;) {
 //		c = to_get_from.get();
-//		if (!to_get_from.eof())
-//		{
+//		if (!to_get_from.eof()) {
 //			ret += c;
-//		}
-//		else // if (to_get_from.eof())
-//		{
+//		} else { // if (to_get_from.eof())
 //			break;
 //		}
 //	}
 //
 //	return ret;
 //}
-inline auto get_istream_as_str(std::istream&& to_get_from)
-{
+inline auto get_istream_as_str(std::istream&& to_get_from) {
 	return get_istream_as_str(to_get_from);
 }
-//inline auto get_istream_as_str(std::ifstream&& to_get_from)
-//{
+//inline auto get_istream_as_str(std::ifstream&& to_get_from) {
 //	return get_istream_as_str(to_get_from);
 //}
 
-inline std::string get_stdin_as_str()
-{
+inline std::string get_stdin_as_str() {
 	return get_istream_as_str(cin);
 }
 
